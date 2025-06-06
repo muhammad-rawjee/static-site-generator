5521f34e3d9ec4dff2ca453be8dae3bda4f143a2
Muhammad Ali Rawjee <muhammadrawjee@gmail.com>
2025-06-06 12:21:14 +0530

    Add ParentNode method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,10 +10,59 @@ class HTMLNode():
     
     def props_to_html(self):
         res = ""
-        for prop in self.props.keys():
-            res += f' {prop}="{self.props[prop]}"'
+        if self.props:
+            for prop in self.props.keys():
+                res += f' {prop}="{self.props[prop]}"'
         return res
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
     
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        else:
+            return f"{self.value}"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    def to_html(self):
+        if not self.tag:
+            raise ValueError
+        if self.children is None:
+            raise ValueError("I am barren, I have no children")
+        # Recursive Soln
+        else:
+            res = ""
+            for child in self.children:
+                res += child.to_html()
+            res = f"<{self.tag}>{res}</{self.tag}>"
+            return res
+        
+# print(LeafNode("p", "This is a paragraph of text.").to_html())
+# "<p>This is a paragraph of text.</p>"
+# print(LeafNode("a", "Click me!", {"href": "https://www.google.com"}).to_html())
+# Expect
+# "<a href="https://www.google.com">Click me!</a>"
+
+# node = ParentNode(
+#     "p",
+#     [
+#         LeafNode("b", "Bold text"),
+#         LeafNode(None, "Normal text"),
+#         LeafNode("i", "italic text"),
+#         LeafNode(None, "Normal text"),
+#     ],
+# )
+
+# print(node.to_html())
+# # EXPECTED
+# # <p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_one(self):
@@ -18,3 +18,41 @@ class TestHTMLNode(unittest.TestCase):
         htmlNode = HTMLNode('a', None, None, {"href": "https://www.apple.com"})
         test = f"href=\"https://www.apple.com\""
         self.assertNotEqual(htmlNode.props_to_html(), test)
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+    
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Click me!")
+        self.assertEqual(node.to_html(), "<h1>Click me!</h1>")
+    
+    def test_parent_to_html_one(self):
+        node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+        )
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
